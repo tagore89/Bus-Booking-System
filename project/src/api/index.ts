70fcf3c676d6d96b9afbe4b6eb75af10361fbe47
Kattukolu Phanindra Tagore <113537739+tagore89@users.noreply.github.com>
2025-05-29 16:57:23 +0530

    Update index.ts

--- project/src/api/index.ts
+++ project/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://bus-booking-system-backend-178v.onrender.com';
 
 // Create axios instance
 const api = axios.create({
